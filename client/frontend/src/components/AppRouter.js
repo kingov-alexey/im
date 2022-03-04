b2b50b39d155df9d553e97628dd3b40061e8291e
kingov.alexey <kingov_alexey@mail.ru>
2022-03-04 22:53:52 +0600

    1,19 add route

--- client/frontend/src/components/AppRouter.js
+++ client/frontend/src/components/AppRouter.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react';
+import React, { useContext } from 'react';
+import { Routes, Route, Redirect } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/consts';
 
 const AppRouter = () => {
-  return <div></div>;
+  const isAuth = false;
+  return (
+    <Routes>
+      {authRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} component={Component} exact />
+      ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} component={Component} exact />
+      ))}
+    </Routes>
+  );
 };
 
 export default AppRouter;
